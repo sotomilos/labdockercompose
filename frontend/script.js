cf12949c64b32aae879cc4acca1250b919d11e3b
Juan Puerta <juan.pablo.puerta@correounivalle.edu.co>
2025-03-30 17:39:31 -0500

    service 5

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const services = [
         { name: "Service 1", url: "http://localhost:5001/api" },
         { name: "Service 2", url: "http://localhost:5002/api" },
-        { name: "Service 3", url: "http://localhost:5003/api" }
+        { name: "Service 3", url: "http://localhost:5003/api" },
+        { name: "Service 5", url: "http://localhost:5005/api" }
     ];
 
     const list = document.getElementById("services-list");
